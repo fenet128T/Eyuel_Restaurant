1c5a7d1b26a9771c18b650a20a4a4230cc503232
fenet128t <fenet128t@gmail.com>
2025-03-26 00:30:06 +0300

    banner committed

--- restaurant/src/App.jsx
+++ restaurant/src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Navbar from "../src/components/Navbar.jsx";
+import Banner from "../src/components/Banner/Banner.jsx";
+
 import Hero from "../src/components/Hero/Hero.jsx";
 const App = () => {
   return (
     <div className="overflow-x-hidden">
       <Navbar />
       <Hero />
-      {/* <Banner />
-      <Services />
+      <Banner />
+      {/* <Services />
       <About handleOrderPopup={handleOrderPopup} />
       <Banner />
       <Footer />
--- restaurant/src/components/Hero/Hero.jsx
+++ restaurant/src/components/Hero/Hero.jsx
@@ -1,17 +1,9 @@
 import React from "react";
 import hero from "../../assets/hero.png";
-import heroBg from "../../assets/heroBg.png";
 import PrimaryButton from "../Shared/PrimaryButton";
-const BgStyle = {
-  backgroundImage: 'url(${heroBg})',
-  backgroundRepeat: "no-repeat",
-  backgroundSize:"center",
-  width: "100%",
-  height:"100%",
-};
 const Hero = () => {
   return (
-    <div style={BgStyle} className="relative z-[-1]">
+    <div className="relative z-[-1]">
       <div className="container py-16 sm:py-0">
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 place-items-center min-h-[600px]">
           {/* text content section */}
@@ -23,7 +15,9 @@ const Hero = () => {
               </span>
               with{" "}
               <span className="text-yellow-600 font-cursive text-7xl">
-                Delivery
+                {" "}
+                <span className="font-cursive">Free{" "}</span>
+                Delivery{" "}
               </span>
               service in Jimma
             </h1>
